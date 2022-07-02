eea2f087c7ca3e96718ebd9934527e8f9ff75d91
Abhishek Chaudhary <43341786+abhishek0220@users.noreply.github.com>
2022-07-02 13:17:59 +0000

    router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,27 +6,40 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMessage } from '@fortawesome/free-solid-svg-icons'
 // import MessageIcon from '@mui/icons-material/Message';
 
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 function App() {
-  const [state, setState] = useState(false)
-
-return (
-
-  <div className="App">
-    <div className="container">
-  
-      <h3 className="Title">SYNC ON</h3>
-    </div>
-    
-
-{state ? <Chat /> : null}
-
-    <button className='btn' onClick={() => setState(true)}><FontAwesomeIcon className='message'icon={faMessage} /></button>
-
-</div>
-
-);
-
+  // return <Home/>
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element = {<Home/>}/>
+          <Route path="/login" element = {<Login/>}/>
+          <Route path="/oauth/callback" element={<Callback/>} />
+        </Routes>
+      </Router>
+    );
 };
 
+function Home() {
+  const [state, setState] = useState(false)
+  return (
+    <div className="App">
+          <div className="container">
+            <h3 className="Title">SYNC ON</h3>
+          </div>
+          {state ? <Chat /> : null}
+          <button className='btn' onClick={() => setState(true)}><FontAwesomeIcon className='message'icon={faMessage} /></button>
+        </div>
+  );
+}
+
+function Login() {
+  return <h2>Login via Spotify</h2>;
+}
+
+function Callback(){
+  return <h2>Callback</h2> 
+}
 
 export default App;
